ba4f25791ade64450be6daea3ee184d8fb1d4b0e
Enrique Ramirez <nrqrmz@gmail.com>
2021-06-17 01:24:54 -0500

    Test app content is displayed

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <br></br>
+        <p>
+          I suppose this line is better than hello world, doesn't it?
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
