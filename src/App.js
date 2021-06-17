78a384287ad0c599d3aec844b81081ef1ad7272c
Enrique Ramirez <nrqrmz@gmail.com>
2021-06-17 01:55:27 -0500

    Todo component added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TodoList from './TodoList';
 
 function App() {
-  return null
+  const [todos, setTodos] = useState(['Todo 1', 'Todo 2'])
+  return (
+    <>
+      <TodoList todos={todos} />
+      <input type="text" />
+      <button>Add Todo</button>
+      <button>Clear Completed Todos</button>
+      <div>0 left to do</div>
+    </>
+  )
 }
 
 export default App;
